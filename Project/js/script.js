e8eef5058d418636d7ab35d03b4975c6113c5964
surewebdev <surewebdev@gmail.com>
2019-12-10 09:59:44 +0530

    register page updated

---
+++ Project/js/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+	$(window).scroll(function () {
+			if ($(this).scrollTop() > 50) {
+				$('#back-to-top').fadeIn();
+			} else {
+				$('#back-to-top').fadeOut();
+			}
+		});
+		// scroll body to 0px on click
+		$('#back-to-top').click(function () {
+			$('body,html').animate({
+				scrollTop: 0
+			}, 400);
+			return false;
+		});
+		// ANIMATE CONTENT
+		$('.specialization-1').waypoint(function() {
+			$('.specialization-1').addClass('fadeInLeft')
+			}, { offset: '50%' });
+});
